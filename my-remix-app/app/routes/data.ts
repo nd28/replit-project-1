23796441d1917a1f251c67e4edbf2919f51937ff
Nilesh <56911917+nd28@users.noreply.github.com>
2024-09-17 03:25:38 +0000

    setup remix app

---
+++ my-remix-app/app/routes/data.ts
@@ -0,0 +1,10 @@
+export function getConfig() {
+  return {
+    title: "My Remix App",
+    description: "A Remix app with data.",
+    author: "Remix",
+    twitter: "remix_run",
+    github: "remix-run",
+    website: "https://remix.run"
+  }
+}
